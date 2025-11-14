d672f770b2b86c4986e05e57b27fd1ea585c6519
Dubois <clement.dubois845@gmail.com>
2025-11-14 10:20:56 +0100

    minio init

---
+++ minio_init/buckets.py
@@ -0,0 +1,29 @@
+from minio import Minio
+import time
+
+# attendre que MinIO démarre
+time.sleep(5)
+
+client = Minio(
+    "minio:9000",
+    access_key="rootuser",
+    secret_key="rootpass123",
+    secure=False
+)
+
+bucketBronze = "bronze"
+bucketSilver = "silver"
+bucketGold = "gold"
+
+# Création des buckets
+if not client.bucket_exists(bucketBronze):
+    client.make_bucket(bucketBronze)
+    print(f"Bucket créé : {bucketBronze}")
+
+if not client.bucket_exists(bucketSilver):
+    client.make_bucket(bucketSilver)
+    print(f"Bucket créé : {bucketSilver}")
+
+if not client.bucket_exists(bucketGold):
+    client.make_bucket(bucketGold)
+    print(f"Bucket créé : {bucketGold}")
